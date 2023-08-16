bf4723d5c7039d764d82e2774bbf63e38b983077
DeviSriCharan20412 <valupadasu20412@iiitd.ac.in>
2023-08-16 19:11:33 +0530

    Bugs Fixed

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -22,6 +22,14 @@ const Demo = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const existingArticle = allArticles.find(
+      (item) => item.url === article.url
+    );
+    if (existingArticle) {
+      setArticle(existingArticle);
+      return;
+    }
+
     const data = await getSummary({ articleUrl: article.url });
 
     if (data?.summary) {
@@ -30,6 +38,7 @@ const Demo = () => {
         summary: data.summary,
       };
       const updatedArticles = [newArticle, ...allArticles];
+
       setArticle(newArticle);
       setAllArticles(updatedArticles);
       localStorage.setItem("articles", JSON.stringify(updatedArticles));
@@ -43,39 +52,50 @@ const Demo = () => {
       setCopied(false);
     }, 2000);
   }
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleSubmit(e);
+    }
+  };
+
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
-        <form className="relative flex justify-center items-center" onSubmit={handleSubmit}>
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}>
           <img src={linkIcon} alt="link icon" className="absolute left-0 my-2 ml-3 w-5" />
           <input
             type="url"
-            placeholder="Enter a URL"
+            placeholder="Paste the article link here..."
             value={article.url}
             onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            onKeyDown={handleKeyDown}
             required
             className="url_input peer"
           />
           <button type="submit" className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700">
-            ↲
+            <p>↲</p>
           </button>
-          {/* Browse URL History */}
-          <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
-            {allArticles.map((article, index) => (
-              <div
-                key={`link-${index}`}
-                onClick={() => setArticle(article)}
-                className="link_card">
-                <div className="copy_btn" onClick={()=>handleCopy(article.url)}>
-                  <img src={copied === article.url ? tick : copy}
-                    alt="copy_icon"
-                    className="w-[40%] h-[40%] object-contain" />
-                  <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">{article.url}</p>
-                </div>
-              </div>
-            ))}
-          </div>
         </form>
+        {/* Browse URL History */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.reverse().map((article, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(article)}
+              className="link_card">
+              <div className="copy_btn" onClick={() => handleCopy(article.url)}>
+                <img
+                  src={copied === article.url ? tick : copy}
+                  alt={copied === article.url ? "tick_icon" : "copy_icon"}
+                  className="w-[40%] h-[40%] object-contain" />
+              </div>
+                <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">{article.url}</p>
+            </div>
+          ))}
+        </div>
       </div>
       {/* Display results */}
       <div className="my-10 max-w-full flex justify-center items-center">
