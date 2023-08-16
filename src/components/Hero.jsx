c0d443b8c977274dc667e1804857f13a3a80bf33
DeviSriCharan20412 <valupadasu20412@iiitd.ac.in>
2023-08-16 19:18:30 +0530

    Added Github Link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
         <img src={logo} alt='sumzLogo' className='w-28 object-contain'/>
-        <button type='button' onClick={()=>{window.open("")}} className='black_btn'>Github</button>
+        <button type='button' onClick={()=>{window.open("https://github.com/devisri-charan/Sumz")}} className='black_btn'>Github</button>
       </nav>
 
       <h1 className='head_text'>Summarize Articles with <br className='max-md:hidden'/> <span className='orange_gradient'>OpenAI GPT-4</span></h1>
